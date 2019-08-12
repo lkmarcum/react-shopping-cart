2480137fd92db6ba3ec0b94ac19f77c640284759
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-12 14:34:34 -0500

    refactored ShappingCart.js & Navigation.js to use Context API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from "./components/ShoppingCart";
 
 // Contexts
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -20,12 +21,14 @@ function App() {
 
   return (
     <ProductContext.Provider value={{ products, addItem }}>
-      <div className="App">
-        <Navigation cart={cart} />
-        {/* Routes */}
-        <Route exact path="/" component={Products} />} />
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
-      </div>
+      <CartContext.Provider value={cart}>
+        <div className="App">
+          <Navigation />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />} />
+          <Route path="/cart" component={ShoppingCart} />
+        </div>
+      </CartContext.Provider>
     </ProductContext.Provider>
   );
 }
